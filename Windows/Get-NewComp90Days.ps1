e6f1a0b69f08317238b3cbcac1c7e00a32920f1d
krosbonz@outlook.com <mhauck@lawrencegeneral.org>
2019-09-13 09:59:14 -0400

    Changed aliases

--- Windows/Get-NewComp90Days.ps1
+++ Windows/Get-NewComp90Days.ps1
@@ -2,5 +2,5 @@ import-module ActiveDirectory
 # Find computers created 90 days ago
 $time = (get-date).AddDays(-45)
 $comp = Get-ADComputer -Filter { createTimeStamp -gt $time  } -Properties *
-$comp | ft name,LastLogonDate,IPv4Address,enabled,CanonicalName -AutoSize
-$comp | select name,LastLogonDate,IPv4Address,enabled,CanonicalName | Out-GridView
+$comp | Format-Table name,LastLogonDate,IPv4Address,enabled,CanonicalName -AutoSize
+$comp | Select-Object name,LastLogonDate,IPv4Address,enabled,CanonicalName | Out-GridView
