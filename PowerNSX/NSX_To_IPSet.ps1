b1d24d6994f991572cc410bcefa3dda063e1e15a
krosbonz@outlook.com <mhauck@lawrencegeneral.org>
2019-09-13 09:07:55 -0400

    Retest

--- PowerNSX/NSX_To_IPSet.ps1
+++ PowerNSX/NSX_To_IPSet.ps1
@@ -1,2 +1,1 @@
-﻿Test
-Get-NsxFirewallSection "App - SCE" | New-NsxFirewallRule -name "SCE-App to Printing" -Source (Get-NSXsecuritygroup sg-sce-app) -Destination (Get-NSXIPSet ip-printing)   -Action Allow
+﻿Get-NsxFirewallSection "App - SCE" | New-NsxFirewallRule -name "SCE-App to Printing" -Source (Get-NSXsecuritygroup sg-sce-app) -Destination (Get-NSXIPSet ip-printing)   -Action Allow
