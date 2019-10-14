a1ee75a005435fc38549004b5865b330c3cd241a
Michael Hauck <krosbonz@outlook.com>
2019-10-14 09:16:16 -0400

    Dev environment

--- Azure/Build_Dev_Env.ps1
+++ Azure/Build_Dev_Env.ps1
@@ -1,2 +1,7 @@
 
-New-AzResourceGroup dev-docker "East US"
+# New-AzResourceGroup rgdevdocker "East US"
+# New-AzStorageAccount -ResourceGroupName rgdevdocker -Name sgdevdocker -Location 'East US' -SkuName Standard_LRS
+Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
+
+$newSubnet = New-AzVirtualNetworkSubnetConfig -Name "snetdevdocker" -AddressPrefix "10.26.4.0/24" 
+New-AzVirtualNetwork -Name vnetdevdocker -ResourceGroupName rgdevdocker -Location "East US" -AddressPrefix "10.26.0.0/16" -Subnet $newSubnet
